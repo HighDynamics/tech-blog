e6421061fe73ad2f0fd820c044c13be99ac9a006
Daniel Denton <danielryandenton@gmail.com>
2021-12-19 16:09:52 -0600

    add styling

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,4 +2,18 @@ function addLineBreaks(content) {
   return content.replaceAll('\n', '<br>');
 }
 
-module.exports = { addLineBreaks };
+function formatDate(date) {
+  return `${new Date(date).getMonth() + 1}/${new Date(
+    date
+  ).getDate()}/${new Date(date).getFullYear()}`;
+}
+
+function formatPlural(word, amount) {
+  if (amount !== 1) {
+    return `${word}s`.toLowerCase();
+  }
+
+  return word.toLowerCase();
+}
+
+module.exports = { addLineBreaks, formatDate, formatPlural };
